5fd5ad5cce2d2bce5f4ce6bb1b6bf990dcd0bc2a
Filipe Suhett <filipe.gmx@gmail.com>
2022-08-21 19:06:00 -0300

    .

---
+++ 01. Listas e Matrizes/02. Exercícios/17 Criar Matriz.py
@@ -0,0 +1,7 @@
+def c_matriz(n,m):
+    l = [n, m]
+
+    return l   
+
+if __name__ == "__c_matriz__":
+    c_matriz()
---
+++ 01. Listas e Matrizes/02. Exercícios/18 Imprimir Matriz.py
@@ -0,0 +1,7 @@
+def i_matriz(n,m):
+    l = [n, m]
+
+    print(l)   
+
+if __name__ == "__i_matriz__":
+    i_matriz()
