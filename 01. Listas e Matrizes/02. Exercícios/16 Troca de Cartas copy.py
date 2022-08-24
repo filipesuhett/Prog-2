604c5cb90a1e62db3675cd5086f309bfd0be02b9
Filipe Suhett <filipe.gmx@gmail.com>
2022-08-24 13:36:10 -0300

    Update 16 Troca de Cartas copy.py

--- 01. Listas e Matrizes/02. Exercícios/16 Troca de Cartas copy.py
+++ 01. Listas e Matrizes/02. Exercícios/16 Troca de Cartas copy.py
@@ -1,32 +1,25 @@
-def Troca(l1, l2):
+def troca(l1, l2):
     lb = []
     la = []
     clear = []
-    i = 0
-    cont = 0
-    
+
     if l1 == l2:
         print(0)
     else:
-        while i < len(l1):
-            while cont < len(l2):    
-                if not l2[cont] == l1[i]:
-                    if lb.count(l1[i]) == 0:
-                        lb.append(l1[i])
-                    if la.count(l2[cont]) == 0:
-                        la.append(l2[cont])
+        for i in l1:
+            for j in l2:    
+                if not i == j:
+                    if lb.count(i) == 0:
+                        lb.append(i)
+                    if la.count(j) == 0:
+                        la.append(j)
                 else:
-                    if clear.count(l1[i]) == 0:
-                        clear.append(l1[i])
-                cont += 1
-            cont = 0
-            i += 1
-        cont = 0
-        if len(clear) > 0:    
-            while len(clear) > cont:
-                la.remove(clear[cont])
-                lb.remove(clear[cont])
-                cont += 1
+                    if clear.count(i) == 0:
+                        clear.append(i)
+        if len(clear) > 0:
+            for b in range(len(clear)):
+                la.remove(clear[b])
+                lb.remove(clear[b])
         if len(la) > len(lb):
             print(len(lb))
         else:
