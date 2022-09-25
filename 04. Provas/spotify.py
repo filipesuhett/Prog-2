ff024009de705b833f550285f366f8b9384aa9b1
Filipe Suhett <filipe.gmx@gmail.com>
2022-09-25 20:28:25 -0300

    Update spotify.py

--- 04. Provas/spotify.py
+++ 04. Provas/spotify.py
@@ -1,3 +1,41 @@
+def spo1(playlists, musicas, usuarios):
+    dura = []
+    dura1 = []
+    
+    for i in range(len(playlists)):
+        som = 0
+        sos = 0
+        for j in range(len(playlists[i][2])):
+            som += musicas[playlists[i][2][j]][2][0]
+            sos += musicas[playlists[i][2][j]][2][1]
+        tup = (som,sos)
+        dura.append(tup)
+    
+    for k in range(len(dura)):
+        if dura[k][1] >= 60:
+            sos = dura[k][1]%60
+        som = dura[k][0] + dura[k][1]//60
+        if dura[k][0] >= 60:
+            som = dura[k][0]%60
+            soh = dura[k][0]//60
+            tup = (soh, som, sos)
+            dura1.append(tup)
+        else:
+            soh = 0
+            tup = (soh, som, sos)
+            dura1.append(tup)
+        if sos < 10:
+            sos = str(sos)
+            sos = '0' + sos
+        if som < 10:
+            som = str(som)
+            som = '0' + som
+        if soh < 10:
+            soh = str(soh)
+            soh = '0' + soh       
+            
+        print(f'{playlists[k][0]} (by {usuarios.get(playlists[k][1])}: {soh}:{som}:{sos}')
+
 def spo2(mu, playlists):
     cont = 0
     
@@ -23,6 +61,6 @@ def spo3(playlists, musicas, artistas):
             te = cont
             maior = x[l]
         
-        l = artistas.get(musicas[maior][0])    
+        l = artistas.get(musicas[maior][0])
             
     print(f'Hit do Ano: {musicas[maior][1]} ({l})')
